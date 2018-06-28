eb1eea9d3f941066c93b842090cb9942ca1a56d6
Mayra Romero <nahytar@gmail.com>
2018-06-28 00:21:54 -0400

    se logra consulta de saldo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 const areValidCredentials = (inputEmail, inputPass) => {
   return inputEmail.length > 3 && inputPass.length > 8;
 }
+
+const login = (event) => {
+  event.preventDefault();
+  if (areValidCredentials(event.target[0].value, event.target[1].value)) {
+    window.location = './indexMenu.html';
+  }else {
+    alert("correo electronico o contraseña invalida")
+  }
+}
+
+window.onload = () => {
+  let loginForm = document.getElementById("loginForm");
+  loginForm.addEventListener("submit", login);
+}
