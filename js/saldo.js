e74618eb7310f44238beb3801cd83994e0cc1db4
nahytar <nahytar@gmail.com>
2018-07-03 23:30:48 -0400

    se usa local storage para guardar numero de tarjeta

--- js/saldo.js
+++ js/saldo.js
@@ -1,15 +1,17 @@
 window.onload = () => {
-  let inputText = document.getElementById('cardNum')
+  let inputText = document.getElementById('cardNum');
+  inputText.value = localStorage.getItem('cardNum');
   inputText.addEventListener('keypress', () => {
+    // 
     let key = event.which || event.keyCode;
     if (key === 13) {
       let cardNum = inputText.value;
-      inputText.value = '';
+      // inputText.value = '';
+      localStorage.setItem('cardNum', cardNum)
 
       fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${cardNum}`)
         .then(response => response.json())
         .then(data => {
-          console.log(data);
           document.getElementById('numTar').innerHTML = data['N&ordm; tarjeta bip! '];
           document.getElementById('saldo').innerHTML = data['Saldo  tarjeta'];
           document.getElementById('fecha').innerHTML = data['Fecha saldo'];
