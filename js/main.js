f060cab270d13e3915ae7fdde34f8941940179f1
nahytar <nahytar@gmail.com>
2018-07-22 23:03:51 -0400

    Estructura HTML

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-const areValidCredentials = (inputEmail, inputPass) => {
-  return inputEmail.length > 3 && inputPass.length > 8;
-}
-
-const login = (event) => {
-  event.preventDefault();
-  if (areValidCredentials(event.target[0].value, event.target[1].value)) {
-    window.location = './indexMenu.html';
-  }else {
-    alert("correo electronico o contraseña invalida")
-  }
-}
-
-window.onload = () => {
-  let loginForm = document.getElementById("loginForm");
-  loginForm.addEventListener("submit", login);
-}
+// const areValidCredentials = (inputEmail, inputPass) => {
+//   return inputEmail.length > 3 && inputPass.length > 8;
+// }
+// const login = (event) => {
+//   let loginForm = document.getElementById("loginForm");
+//   loginForm.addEventListener("submit", login);
+//     event.preventDefault();
+//     if (areValidCredentials(event.target[0].value, event.target[1].value)) {
+//       window.location = './indexMenu.html';
+//       if (){
+        
+//       }
+//     } else {
+//       alert("correo electronico o contraseña invalida");
+//     }
+//   }
--- js/saldo.js
+++ js/saldo.js
@@ -1,22 +1,22 @@
-window.onload = () => {
-  let inputText = document.getElementById('cardNum');
-  inputText.value = localStorage.getItem('cardNum');
-  inputText.addEventListener('keypress', () => {
-    // 
-    let key = event.which || event.keyCode;
-    if (key === 13) {
-      let cardNum = inputText.value;
-      // inputText.value = '';
-      localStorage.setItem('cardNum', cardNum)
+// window.onload = () => {
+//   let inputText = document.getElementById('cardNum');
+//   inputText.value = localStorage.getItem('cardNum');
+//   inputText.addEventListener('keypress', () => {
+//     // 
+//     let key = event.which || event.keyCode;
+//     if (key === 13) {
+//       let cardNum = inputText.value;
+//       // inputText.value = '';
+//       localStorage.setItem('cardNum', cardNum)
 
-      fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${cardNum}`)
-        .then(response => response.json())
-        .then(data => {
-          document.getElementById('numTar').innerHTML = data['N&ordm; tarjeta bip! '];
-          document.getElementById('saldo').innerHTML = data['Saldo  tarjeta'];
-          document.getElementById('fecha').innerHTML = data['Fecha saldo'];
-          document.getElementById('estado').innerHTML = data['Estado de contrato'];
-        })
-    }
-  })
-}
+//       fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${cardNum}`)
+//         .then(response => response.json())
+//         .then(data => {
+//           document.getElementById('numTar').innerHTML = data['N&ordm; tarjeta bip! '];
+//           document.getElementById('saldo').innerHTML = data['Saldo  tarjeta'];
+//           document.getElementById('fecha').innerHTML = data['Fecha saldo'];
+//           document.getElementById('estado').innerHTML = data['Estado de contrato'];
+//         })
+//     }
+//   })
+// }
